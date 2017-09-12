32aebbc5c3c7d9f75d614c1ad6fa4ed053d75170
jandoant <jandoant@gmail.com>
2017-09-12 13:09:47 +0200

    adding image and html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var extractPlugin = new ExtractTextPlugin({
     filename: 'main.css'
+});
+
+var htmlPlugin = new HtmlWebpackPlugin({
+    template: 'src/index.html'
 });
 
 module.exports = {
@@ -10,10 +15,12 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
-        publicPath: '/build'
+        publicPath: './'
     },
     module: {
-        rules: [{
+        rules: [
+            //Transpile Javascript ES6 to ES5 
+            {
                 test: /\.js$/,
                 use: [{
                     loader: 'babel-loader',
@@ -22,15 +29,34 @@ module.exports = {
                     }
                 }]
             },
+            //Transpile SCSS to CSS  
             {
-                test: /\.scss$/,
+                test: /\.(scss|sass)$/,
                 use: extractPlugin.extract({
                     use: ['css-loader', 'sass-loader']
                 })
+            },
+            //HTML-loader
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            //Image-Loader
+            {
+                test: /\.(jpg|jpeg|png|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img/',
+                        publicPath: 'img/'
+                    }
+                }]
             }
         ]
     },
     plugins: [
-        extractPlugin
+        extractPlugin,
+        htmlPlugin
     ]
 };
